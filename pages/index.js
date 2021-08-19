f8cd890323f1d18f29690d390f82fff6e1349c96
wandersonce <wandersonce1@hotmail.com>
2021-08-19 15:58:32 -0700

    Implementing styles

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,9 @@ export default function Home() {
       <div>
         <h1> Products </h1>
         <ul>
-          <li> Product 1 </li> <li> Product 2 </li> <li> Product 3 </li>
+          <li> Product 1 </li>
+          <li> Product 2 </li>
+          <li> Product 3 </li>
         </ul>
       </div>
     </Layout>
